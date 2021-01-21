514c867da94a1a22707a1a89328b6926a5f9f0d3
nodebreaker0-0 <zeroday26@gmail.com>
2021-01-21 15:20:37 +0900

    tx gun code edit

--- grpc-example/main.go
+++ grpc-example/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	clienttx "github.com/cosmos/cosmos-sdk/client/tx"
 	"github.com/cosmos/cosmos-sdk/crypto"
@@ -34,12 +35,13 @@ func queryState() error {
 		grpc.WithInsecure(),
 		grpc.WithBlock(), // The SDK doesn't support any transport security mechanism.
 	)
-	defer grpcConn.Close()
-
+	//defer grpcConn.Close()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
 	// This creates a gRPC client to query the x/bank service.
 	bankClient := banktypes.NewQueryClient(grpcConn)
 	bankRes, err := bankClient.AllBalances(
-		context.Background(),
+		ctx,
 		&banktypes.QueryAllBalancesRequest{Address: myAddress.String()},
 	)
 	if err != nil {
@@ -51,7 +53,7 @@ func queryState() error {
 	// This creates a gRPC client to query the x/bank service.
 	authClient := authtypes.NewQueryClient(grpcConn)
 	authRes, err := authClient.Account(
-		context.Background(),
+		ctx,
 		&authtypes.QueryAccountRequest{Address: myAddress.String()},
 	)
 	if err != nil {
@@ -73,8 +75,11 @@ func banksend() error {
 		grpc.WithInsecure(),
 		grpc.WithBlock(), // The SDK doesn't support any transport security mechanism.
 	)
-	defer grpcConn.Close()
 
+	//ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+
+	defer grpcConn.Close()
+	//defer cancel()
 	keyring, err := keys.New("swapchain", "os", "/root/.liquidityd/", nil)
 
 	account1, err := keyring.Key("user1") //paswd
@@ -93,8 +98,8 @@ func banksend() error {
 
 	encCfg := simapp.MakeTestEncodingConfig()
 	txBuilder := encCfg.TxConfig.NewTxBuilder()
-	msg1 := banktypes.NewMsgSend(addr1, addr2, types.NewCoins(types.NewInt64Coin("uatom", 10000000)))
-	msg2 := banktypes.NewMsgSend(addr2, addr1, types.NewCoins(types.NewInt64Coin("uusdt", 10000000)))
+	msg1 := banktypes.NewMsgSend(addr1, addr2, types.NewCoins(types.NewInt64Coin("uatom", 1000000)))
+	msg2 := banktypes.NewMsgSend(addr2, addr1, types.NewCoins(types.NewInt64Coin("uusdt", 1000000)))
 
 	err = txBuilder.SetMsgs(msg1, msg2)
 	if err != nil {
@@ -134,74 +139,83 @@ func banksend() error {
 	if err != nil {
 		return err
 	}
-
 	accNums := []uint64{acc1.AccountNumber, acc2.AccountNumber} // The accounts' account numbers
 	accSeqs := []uint64{acc1.Sequence, acc2.Sequence}           // The accounts' sequence numbers
-
-	// First round: we gather all the signer infos. We use the "set empty
-	// signature" hack to do that.
-	var sigsV2 []signing.SignatureV2
-	for i, priv := range privs {
-		sigV2 := signing.SignatureV2{
-			PubKey: priv.PubKey(),
-			Data: &signing.SingleSignatureData{
-				SignMode:  encCfg.TxConfig.SignModeHandler().DefaultMode(),
-				Signature: nil,
-			},
-			Sequence: accSeqs[i],
-		}
-
-		sigsV2 = append(sigsV2, sigV2)
-	}
-
-	err = txBuilder.SetSignatures(sigsV2...)
-	if err != nil {
-		return err
-	}
-
-	// Second round: all signer infos are set, so each signer can sign.
-	sigsV2 = []signing.SignatureV2{}
-	for i, priv := range privs {
-		signerData := xauthsigning.SignerData{
-			ChainID:       "swap-testnet-2001",
-			AccountNumber: accNums[i],
-			Sequence:      accSeqs[i],
-		}
-		sigV2, err := clienttx.SignWithPrivKey(
-			encCfg.TxConfig.SignModeHandler().DefaultMode(), signerData,
-			txBuilder, priv, encCfg.TxConfig, accSeqs[i])
-		if err != nil {
-			return err
-		}
-
-		sigsV2 = append(sigsV2, sigV2)
-	}
-	err = txBuilder.SetSignatures(sigsV2...)
-	if err != nil {
-		return err
-	}
-	txBytes, err := encCfg.TxConfig.TxEncoder()(txBuilder.GetTx())
-	if err != nil {
-		return err
-	}
+	startTime := time.Now()
+	count := 0
 
 	// Broadcast the tx via gRPC. We create a new client for the Protobuf Tx
 	// service.
 	txClient := tx.NewServiceClient(grpcConn)
-	// We then call the BroadcastTx method on this client.
-	grpcRes, err := txClient.BroadcastTx(
-		context.Background(),
-		&tx.BroadcastTxRequest{
-			Mode:    tx.BroadcastMode_BROADCAST_MODE_SYNC,
-			TxBytes: txBytes, // Proto-binary of the signed transaction, see previous step.
-		},
-	)
-	if err != nil {
-		return err
+	for q := 0; q < 200; q++ {
+		count = count + 1
+		println(accSeqs[0], accSeqs[1])
+		println(count)
+		// First round: we gather all the signer infos. We use the "set empty
+		// signature" hack to do that.
+		var sigsV2 []signing.SignatureV2
+		for i, priv := range privs {
+			sigV2 := signing.SignatureV2{
+				PubKey: priv.PubKey(),
+				Data: &signing.SingleSignatureData{
+					SignMode:  encCfg.TxConfig.SignModeHandler().DefaultMode(),
+					Signature: nil,
+				},
+				Sequence: accSeqs[i],
+			}
+
+			sigsV2 = append(sigsV2, sigV2)
+		}
+
+		err = txBuilder.SetSignatures(sigsV2...)
+		if err != nil {
+			return err
+		}
+
+		// Second round: all signer infos are set, so each signer can sign.
+		sigsV2 = []signing.SignatureV2{}
+		for i, priv := range privs {
+			signerData := xauthsigning.SignerData{
+				ChainID:       "swap-testnet-2001",
+				AccountNumber: accNums[i],
+				Sequence:      accSeqs[i],
+			}
+			sigV2, err := clienttx.SignWithPrivKey(
+				encCfg.TxConfig.SignModeHandler().DefaultMode(), signerData,
+				txBuilder, priv, encCfg.TxConfig, accSeqs[i])
+			if err != nil {
+				return err
+			}
+			accSeqs[i] = accSeqs[i] + 1
+			sigsV2 = append(sigsV2, sigV2)
+		}
+		err = txBuilder.SetSignatures(sigsV2...)
+		if err != nil {
+			return err
+		}
+		txBytes, err := encCfg.TxConfig.TxEncoder()(txBuilder.GetTx())
+		if err != nil {
+			return err
+		}
+
+		// We then call the BroadcastTx method on this client.
+		grpcRes, err := txClient.BroadcastTx(
+			context.Background(),
+			&tx.BroadcastTxRequest{
+				Mode:    tx.BroadcastMode_BROADCAST_MODE_ASYNC,
+				TxBytes: txBytes, // Proto-binary of the signed transaction, see previous step.
+			},
+		)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(grpcRes.TxResponse.Code) // Should be `0` if the tx is successful
+
 	}
+	elapsedTime := time.Since(startTime)
 
-	fmt.Println(grpcRes.TxResponse.Code) // Should be `0` if the tx is successful
-
+	fmt.Printf("실행시간: %s\n", elapsedTime)
 	return nil
 
 }
